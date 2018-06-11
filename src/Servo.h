76716f267de9b4fd70a157167cc3c1bd45b3be85
Thomas Lorentzen <thomas@tlorentzen.net>
2018-06-11 12:50:03 +0200

    Library updated
    We had to update the frekvency to uptimize frekvencies sent to ESC modules.

--- src/Servo.h
+++ src/Servo.h
@@ -75,12 +75,12 @@
 
 #define Servo_VERSION           2     // software version of this library
 
-#define MIN_PULSE_WIDTH       544     // the shortest pulse sent to a servo  
-#define MAX_PULSE_WIDTH      2400     // the longest pulse sent to a servo 
-#define DEFAULT_PULSE_WIDTH  1500     // default pulse width when servo is attached
-#define REFRESH_INTERVAL    20000     // minumim time to refresh servos in microseconds 
+#define MIN_PULSE_WIDTH       500     // the shortest pulse sent to a servo  
+#define MAX_PULSE_WIDTH      2000     // the longest pulse sent to a servo 
+#define DEFAULT_PULSE_WIDTH  1000     // default pulse width when servo is attached
+#define REFRESH_INTERVAL     8000     // minumim time to refresh servos in microseconds 
 
-#define SERVOS_PER_TIMER       12     // the maximum number of servos controlled by one timer 
+#define SERVOS_PER_TIMER        4     // the maximum number of servos controlled by one timer 
 #define MAX_SERVOS   (_Nbr_16timers  * SERVOS_PER_TIMER)
 
 #define INVALID_SERVO         255     // flag indicating an invalid servo index
